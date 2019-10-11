563d8a84368f0211bc24de49211dc6d154eaaffa
thatcarguy <5158433+thatcarguy@users.noreply.github.com>
2019-10-11 00:14:19 -0600

    Changes for the starter jpa webapp

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -1,19 +1,27 @@
 package guru.springframework.spring5webapp.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by jt on 5/16/17.
  */
-
+@Entity
 public class Author {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy="authors")
     private Set<Book> books  = new HashSet<>();
 
     public Author() {
@@ -61,4 +69,36 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
+	}
+    
+    
 }
